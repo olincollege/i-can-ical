c33e9b8feb66aa226f1f8f08a40c66b38c337c67
Isha Goyal <goyalisha03@gmail.com>
2022-05-03 13:01:37 -0400

    Populated model and started on view.

--- icanical_model.py
+++ icanical_model.py
@@ -1,4 +1,46 @@
 """
-Model of i-can-cal. Model is the information that goes into making an ical.
+Model of i-can-cal. It holds the attributes of an event, which can then be used
+to create and send an ical.
 """
 
+class model():
+    """
+    Model that holds event information.
+
+    Attributes:
+
+    """
+
+    def __init__(self, event_name, start_datetime, end_datetime, attendee_email, organizer_email=None, location=None):
+        self._name = event_name
+        self._start_datetime = start_datetime
+        self._end_datetime = end_datetime
+        self._attendee = attendee_email
+        self._organizer = organizer_email
+        self._location = location
+
+    # defined below are a list of properties so that these values can be accessed but not changed by other functions
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def start(self):
+        return self._start_datetime
+
+    @property
+    def end(self):
+        return self._end_datetime
+    
+    @property
+    def attendee(self):
+        return self._attendee
+
+    @property
+    def organizer(self):
+        return self._organizer
+
+    @property
+    def location(self):
+        return self._location
+    
--- icanical_view.py
+++ icanical_view.py
@@ -1,1 +1,17 @@
-# This will be the view of our code
+"""
+The view for ican ical.
+"""
+
+class view():
+    """
+    The view for ican ical draws from the model to create and send an ics file
+    to the user.
+    """
+
+    def __init__(self, model):
+        self._model = model
+        self._filepath = f'test_icals/{model.name}.ics'
+
+    def send_ical(self):
+        pass
+    # put in the other two functions and also change their inputs.
